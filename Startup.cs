5f5689d117ddb688b88474f4e0df689602e65960
Jon Hilton <hilton.jon@gmail.com>
2020-02-07 10:25:29 +0000

    A dismissable banner

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using BlazorExamples.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -23,6 +24,7 @@ namespace BlazorExamples
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddBlazoredLocalStorage();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
